728458fa2eba97567f48f27b6f666e266c0334eb
kaustavjit <skaustavjit@gmail.com>
2021-05-31 01:42:32 +0530

    Reversed "Revert "Initial Commit"""
    This reverts commit 51a5570b48eb3a2eb16be47173bef2c43851219f.

--- bankinfo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
-from graphene_django.views import GraphQLView
-from bankinfo.schema import schema
-
-urlpatterns = [
-
-    path('gql',GraphQLView.as_view(graphiql=True,schema=schema)),
-]
