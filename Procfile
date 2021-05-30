280f8ee13e3b2a3459a0519c601819f142bad535
kaustavjit <skaustavjit@gmail.com>
2021-05-31 01:15:30 +0530

    Revert "Initial Commit"
    This reverts commit 8cbcc278e5be598c48cefb55e2cdebaab33b813e.

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn indian_banks.wsgi --log-file -
--- bankinfo/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Banks,Branches
-
-# Register your models here.
-admin.site.register(Banks)
-admin.site.register(Branches)
